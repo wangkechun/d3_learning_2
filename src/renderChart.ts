812084cedb386538179f86c5e9c899d4d8da1bee
caohuilin <1057275848@qq.com>
2020-05-01 16:35:00 +0800

    动态更新数据

--- src/renderChart.ts
+++ src/renderChart.ts
@@ -1,22 +1,27 @@
 import * as d3 from 'd3'
 
+// 每一个矩形高度
+const rectHeight = 25
+
 function renderChart(dom: HTMLDivElement, data: number[]) {
-  console.log("renderChart", data)
-  const svg = d3
-    .select(dom)
-    .append('svg')
-    .attr('width', dom.clientWidth)
-    .attr('height', dom.clientHeight)
-  const rectHeight = 25
-  svg
-    .selectAll('rect')
-    .data(data)
-    .enter()
-    .append('rect')
+  // 生成画布
+  let svg = d3.select(dom).select('svg')
+  if (svg.empty()) {
+    d3.select(dom)
+      .append('svg')
+      .attr('width', dom.clientWidth)
+      .attr('height', dom.clientHeight)
+  }
+
+  const rects = svg.selectAll('rect').data(data)
+  rects.enter().append('rect')
+  rects
+    .merge(rects)
     .attr('height', `${rectHeight}px`)
     .attr('width', (data) => `${data}px`)
     .attr('y', (data, index) => `${index * (rectHeight + 10)}px`)
     .attr('fill', 'steelblue')
-  svg.exit().remove()
+
+  rects.exit().remove()
 }
 export default renderChart
