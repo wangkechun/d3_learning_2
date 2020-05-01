6cbd901a23d0419be5c76e4188193a27c3461343
caohuilin <1057275848@qq.com>
2020-05-01 15:24:54 +0800

    hello world

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import * as d3 from 'd3'
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component<{}, {}> {
+  ref: HTMLDivElement | null = null
+
+  setRef = (ref: HTMLDivElement | null) => {
+    this.ref = ref
+  }
+
+  componentDidMount() {
+    d3.select(this.ref).append('div').text('hello d3!')
+  }
+  render() {
+    return <div ref={this.setRef}></div>
+  }
 }
-
-export default App;
