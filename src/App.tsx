b1a6bef8003ed74a4d9e96f5d46c59bb3f550c49
caohuilin <1057275848@qq.com>
2020-05-02 11:01:33 +0800

    delete code

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import { renderChart, renderChartFunc, renderChartLine } from './renderChart'
 import './App.css'
-import { lineOption } from './constants'
 
 export default class App extends React.Component<{}, {}> {
   ref: HTMLDivElement | null = null
@@ -11,44 +10,7 @@ export default class App extends React.Component<{}, {}> {
     this.ref = ref
   }
 
-  demo1() {
-    const data = [10, 15, 30, 50, 80, 65, 55, 30, 20, 10, 8]
-    this.time = setInterval(() => {
-      data.shift()
-      data.push(Math.round(Math.random() * 100))
-      if (this.ref) {
-        renderChart(this.ref, data)
-      } else {
-        console.error('dom not exit')
-      }
-    }, 1500)
-  }
-
-  demo2() {
-    const data = (x: number) => {
-      return x * x + 15
-    }
-    this.time = setInterval(() => {
-      if (this.ref) {
-        renderChartFunc(this.ref, data)
-      } else {
-        console.error('dom not exit')
-      }
-    }, 1500)
-  }
-
-  demoLine() {
-    if (this.ref) {
-      renderChartLine(this.ref, lineOption)
-    } else {
-      console.error('dom not exit')
-    }
-  }
-
   componentDidMount() {
-    // this.demo1()
-    // this.demo2()
-    this.demoLine()
   }
 
   componentWillUnmount() {
